db00c2850e3ee85662e70a99a2738d3b59cda898
AABozhikov19 <AABozhikov19@codingburgas.bg>
2023-04-26 08:45:38 +0300

    Add lambda

---
+++ lambda/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String[] input=sc.nextLine().split(", ");
+        List<Integer> integers=new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            integers.add(Integer.parseInt(input[i]));
+        }
+        integers.removeIf(e-> e%2!=0);
+        integers.forEach(e-> System.out.print(e));
+        integers.sort((x,y)->x.compareTo(y));
+        System.out.println();
+        integers.forEach(e-> System.out.print(e));
+//        for (int i = 0; i < integers.size(); i++) {
+//            if(integers.get(i)%2==0)
+//            {
+//                System.out.print(integers.get(i)+" ");
+//            }
+//        }
+//        Collections.sort(integers);
+//        for (int i = 0; i < integers.size(); i++) {
+//            if(integers.get(i)%2==0)
+//            {
+//                System.out.println(integers.get(i)+" ");
+//            }
+//        }
+    }
+}
